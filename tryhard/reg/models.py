aa1b170a8791f44216e7012f846813e4fe06fc71
ittipat02 <ittipatpoon2@gmail.com>
2021-09-13 03:02:10 +0700

    i just add all file i have

--- tryhard/reg/models.py
+++ tryhard/reg/models.py
@@ -3,19 +3,23 @@ from django.db import models
 # Create your models here.
 
 
+
 class Student(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
-    student_id = models.IntegerField()
+    student_id = models.IntegerField(null=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} {self.student_id}"
 
-
 class Course(models.Model):
+    course_id = models.IntegerField(null=True)
     course_name = models.CharField(max_length=64)
-    student_list = models.ForeignKey(
-        Student, on_delete=models.CASCADE, related_name="member")
+    student_list = models.ManyToManyField(Student, blank=True, related_name="subjects")
+
+    
+  
 
     def __str__(self):
-        return f"{self.course_name} have {self.student_list} "
+        return f"{self.course_id} {self.course_name} "
+
--- tryhard/reg/urls.py
+++ tryhard/reg/urls.py
@@ -1,8 +1,13 @@
+from reg.models import Course
 from django.urls import path
 
 from . import views
 
 app_name = "reg"
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('<int:course_id>', views.course,  name="course"),
+    path('<int:course_id>/addStudent', views.addStudent, name="addStudent"),
+    #path('<int:student_id>', views.student, name="student"),
+
 ]
